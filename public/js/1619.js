7c41c4edb40c019f15d9d6b06765b25d1796cd10
Allan Pichardo <allan.pichardo@gmail.com>
2020-04-14 15:01:30 -0400

    first commit

---
+++ public/js/1619.js
@@ -0,0 +1,320 @@
+let mapStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ebe3cd"
+            }
+        ]
+    },
+    {
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#523735"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#f5f1e6"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#c9b2a6"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#dcd2be"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ae9e90"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dfd2ae"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dfd2ae"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#93817c"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#a5b076"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#447530"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f1e6"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#fdfcf8"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f8c967"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#e9bc62"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e98d58"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#db8555"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#806b63"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dfd2ae"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#8f7d77"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ebe3cd"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dfd2ae"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#b9d3c2"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#92998d"
+            }
+        ]
+    }
+];
+
+let map;
+let socket;
+let poly;
+
+function start() {
+    const bounds = {
+        west: -95.912506,
+        north: 45.930668,
+        east: 19.721023,
+        south: -30.0890
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 20.588594, lng: -43.5690117},
+        zoom: 3.7,
+        styles: mapStyle,
+        disableDefaultUI: true,
+        restriction: {
+            latLngBounds: bounds,
+            strictBounds: false
+        }
+    });
+
+    poly = new google.maps.Polyline({
+        strokeColor: '#000000',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+    });
+    poly.setMap(map);
+
+    socket = io('http://localhost:8080');
+
+    socket.on('draw line', function(line){
+        let path = poly.getPath();
+        path.push(new google.maps.LatLng(line.start));
+        path.push(new google.maps.LatLng(line.end));
+    });
+
+}
+
+function testLines() {
+    setTimeout(() => {
+        socket.emit('segment', 'Colombia,Cuba');
+        setTimeout(() => {
+            socket.emit('segment', 'Cuba,Dominican Republic');
+            setTimeout(() => {
+                socket.emit('segment', 'Dominican Republic,Puerto Rico');
+                setTimeout(() => {
+                    socket.emit('segment', 'Puerto Rico,Kongo');
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+
+}
---
+++ server.js
@@ -0,0 +1,219 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+const locations = {
+  Akan: {
+    lat: 5.5912045,
+    lng: -0.2497705
+  },
+  Alabama: {
+    lat: 32.5562859,
+    lng: -88.9247942
+  },
+  Anguilla: {
+    lat: 18.2206,
+    lng: -63.0686
+  },
+  Bahamas: {
+    lat: 25.0343,
+    lng: -77.3963
+  },
+  Belize: {
+    lat: 17.1899,
+    lng: -88.4976
+  },
+  Benin: {
+    lat: 9.3077,
+    lng: 2.3158
+  },
+  Bermuda: {
+    lat: 32.3078,
+    lng: -64.7505
+  },
+  Brazil: {
+    lat: -14.2350,
+    lng: -51.9253
+  },
+  Chicago: {
+    lat: 41.8781,
+    lng: -87.6298
+  },
+  Colombia: {
+    lat: 4.5709,
+    lng: -74.2973
+  },
+  Cuba: {
+    lat: 21.5218,
+    lng: -77.7812
+  },
+  Detroit: {
+    lat: 42.3314,
+    lng: -83.0458
+  },
+  Dominica: {
+    lat: 15.4150,
+    lng: -61.3710
+  },
+  Dominican_Republic: {
+    lat: 18.7317,
+    lng: -70.1627
+  },
+  Fulani: {
+    lat: 14.4974,
+    lng: 14.4524
+  },
+  Georgia: {
+    lat: 32.1656,
+    lng: -82.9001
+  },
+  Grenada: {
+    lat: 12.1165,
+    lng: -61.6790
+  },
+  Guadeloupe: {
+    lat: 16.2650,
+    lng: -61.5510
+  },
+  Haiti: {
+    lat: 18.9712,
+    lng: -72.2852
+  },
+  Hausa: {
+    lat: 12.2413,
+    lng: 9.9074
+  },
+  Igbo: {
+    lat: 6.9792,
+    lng: 3.9980
+  },
+  Jamaica: {
+    lat: 18.1096,
+    lng: -77.2975
+  },
+  Kanem: {
+    lat: 14.8781,
+    lng: 15.4068
+  },
+  Kangaba: {
+    lat: 11.9428,
+    lng: 8.4157
+  },
+  Kongo: {
+    lat: -4.0383,
+    lng: 21.7587
+  },
+  Louisiana: {
+    lat: 30.9843,
+    lng: -91.9623
+  },
+  Mali: {
+    lat: 17.5707,
+    lng: -3.9962
+  },
+  Mande: {
+    lat: 12.545556,
+    lng: -8.085556
+  },
+  Mexico: {
+    lat: 21.1619,
+    lng: -86.8515
+  },
+  Mississippi: {
+    lat: 32.3547,
+    lng: -89.3985
+  },
+  Nevis: {
+    lat: 17.1554,
+    lng: -62.5796
+  },
+  New_York: {
+    lat: 40.7128,
+    lng: -74.0060
+  },
+  Panama: {
+    lat: 8.5380,
+    lng: -80.7821
+  },
+  Peru: {
+    lat: -9.19,
+    lng: -75.0152
+  },
+  Puerto_Rico: {
+    lat: 18.2208,
+    lng: -66.5901
+  },
+  Saint_Eustatius: {
+    lat: 17.4890,
+    lng: -62.9736
+  },
+  St_Lucia: {
+    lat: 13.9094,
+    lng: -60.9789
+  },
+  Tennessee: {
+    lat: 35.5175,
+    lng: -86.5804
+  },
+  Texas: {
+    lat: 31.9686,
+    lng: -99.9018
+  },
+  Trinidad: {
+    lat: 10.6918,
+    lng: -61.2225
+  },
+  Venezuela: {
+    lat: 6.4238,
+    lng: -66.5897
+  },
+  Virginia: {
+    lat: 37.4316,
+    lng: -78.6569
+  },
+  Wolof: {
+    lat: 15.397663,
+    lng: -15.148774
+  },
+  Yoruba: {
+    lat: 7.4832132,
+    lng: 4.3997974
+  }
+};
+
+function getCoordinates(region) {
+  let underscored = region.replace(' ', '_');
+  return locations[underscored];
+}
+
+io.on('connection', function(socket){
+  socket.on('segment', (msg) => {
+    console.log(`> Received segment ${msg}`);
+    const parts = msg.split(',');
+    let start = getCoordinates(parts[0]);
+    let end = getCoordinates(parts[1]);
+    socket.emit('draw line', {
+      start: start,
+      end: end
+    });
+  });
+});
+
+(async () => {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/main.html');
+  });
+
+  app.post('/segment', (req, res) => {
+
+  });
+
+  app.listen(8000, () => {
+    console.log('> Ready on http://localhost:3000');
+  });
+
+  io.listen(8080);
+})();
