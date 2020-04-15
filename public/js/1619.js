f4495bf0c84d5af6953f26604410d176724b6d7b
Allan Pichardo <allan.pichardo@gmail.com>
2020-04-14 21:05:04 -0400

    drawing the line slightly longer

--- public/js/1619.js
+++ public/js/1619.js
@@ -344,8 +344,8 @@ function animatePath(start, end, durationSeconds = 1.0) {
 
     let animationSequence = () => {
         step += 1;
-        if (step < numSteps) {
-            let t = step/numSteps;
+        let t = step/numSteps;
+        if (t <= 1.0) {
             let are_we_there_yet = google.maps.geometry.spherical.interpolate(start,end,t);
             line.setPath([start, are_we_there_yet]);
             requestAnimationFrame(animationSequence);
